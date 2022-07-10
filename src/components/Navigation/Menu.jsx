398e3b167e57fca0979134657fc581e7bfbf9303
jungmir <ham4051@gmail.com>
2022-07-10 22:57:20 +0900

    react route 적용 및 propTypes 수정

--- src/components/Navigation/Menu.jsx
+++ src/components/Navigation/Menu.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Menu({ menu }) {
   return (
     <div className="NavBarItem block h-full w-full items-center mt-4 mb-4 sm:ml-4 sm:mr-4 sm:mt-0 sm:mb-0">
       {menu.type === 'Logo'
         ? (
-          <a href={menu.url} className="Logo text-opacity-0 flex justify-center items-center sm:h-full">
-            {menu.name}
+          <Link to={menu.url} className="Logo text-opacity-0 flex justify-center items-center sm:h-full">
             <img className="LogoImage flex items-center justify-center min-h-logo min-w-logo" src={menu.img} alt="" />
-          </a>
+          </Link>
         )
-        : <a href={menu.url} className="Menu block w-full h-full"><b className="MenuName flex h-full items-center justify-center">{menu.name}</b></a>}
+        : <Link to={menu.url} className="Menu block w-full h-full"><b className="MenuName flex h-full items-center justify-center">{menu.name}</b></Link>}
     </div>
   );
 }
 
 Menu.propTypes = {
   menu: PropTypes.shape({
-    url: PropTypes.string.isRequired,
+    url: PropTypes.string,
     name: PropTypes.string,
-    type: PropTypes.string.isRequired,
+    type: PropTypes.string,
     img: PropTypes.string,
   }).isRequired,
 };
