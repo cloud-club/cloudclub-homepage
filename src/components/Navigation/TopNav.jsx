c171149be5cfa06c69c257d39cf2150c3e8ef3a0
jungmir <ham4051@gmail.com>
2022-07-10 22:57:07 +0900

    테스트용 더미 데이터 코드 제거

--- src/components/Navigation/TopNav.jsx
+++ src/components/Navigation/TopNav.jsx
@@ -1,46 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Menu from './Menu';
 
-function TopNav() {
-  const menus = [
-    {
-      type: 'Logo',
-      name: '',
-      url: '/',
-      img: 'https://i.ibb.co/Dw3Wz7f/001-1.png',
-    },
-    {
-      type: 'Menu',
-      name: '소개',
-      url: '/intro',
-      img: '',
-    },
-    {
-      type: 'Menu',
-      name: '1기 활동',
-      url: '/intro',
-      img: '',
-    },
-    {
-      type: 'Menu',
-      name: '운영 계획',
-      url: '/intro',
-      img: '',
-    },
-    {
-      type: 'Menu',
-      name: '2기 지원하기',
-      url: '/intro',
-      img: '',
-    },
-  ];
+function TopNav({ menuList }) {
   return (
     <nav className="TopNavBar flow-root bg-white h-full items-center sm:flex">
-      { menus.map((menu) => (
+      { menuList.map((menu) => (
         <Menu menu={menu} />
       ))}
     </nav>
   );
 }
+
+TopNav.propTypes = {
+  menuList: PropTypes.arrayOf(Menu).isRequired,
+};
 
 export default TopNav;
